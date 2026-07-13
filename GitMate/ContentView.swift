22802773d1733c177cf56672660be4f9068bf941
somiljain2006 <somil16022006@gmail.com>
2026-07-13 14:09:51 +0530

    Initial Commit

---
+++ GitMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitMate
+//
+//  Created by somil jain on 13/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitMate/GitMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitMateApp.swift
+//  GitMate
+//
+//  Created by somil jain on 13/07/26.
+//
+
+import SwiftUI
+
+@main
+struct GitMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
