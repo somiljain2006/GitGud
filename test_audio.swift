b17ea3ba7e71f0445b14252def33ab987f8e7b4a
somil jain <somil16022006@gmail.com>
2026-09-15 11:36:59 +0530

    Delete test_audio.swift

--- test_audio.swift
+++
@@ -1,9 +0,0 @@
-import AVFoundation
-
-let session = AVAudioSession.sharedInstance()
-do {
-    try session.setCategory(.record, mode: .measurement, options: .duckOthers)
-    print("setCategory success")
-} catch {
-    print("setCategory failed:", error)
-}
